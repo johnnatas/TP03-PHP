4acd868e7456d7df794d5a1105d1f16ad70a48e8
Leonardo Santos <leonardossev@gmail.com>
2017-05-26 03:58:56 -0300

    [ADD] Bootstrap 4 e logo da Fatec

--- index.php
+++ index.php
@@ -1,7 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jonatas
- * Date: 05/05/17
- * Time: 17:53
- */
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+        <title>Index</title>
+    </head>
+    <body>
+    <div class="container">
+        <nav class="navbar navbar-inverse navbar-toggleable-md bg-inverse">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Fatec PG</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Página Inicial<span class="sr-only">(Current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">#</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">#</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
+            <h1 class="text-center">Trabalho Prático de Linguagem de Programação IV</h1>
+        </div>
+
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
