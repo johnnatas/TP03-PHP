c95e89dc09d090fec279af4476ca4b6302a69377
Jonatas Lopes <jonatas_lopes@outlook.com>
2017-05-31 20:29:04 -0300

    estruturação básica

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
     <head>
         <meta charset="utf-8">
         <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-        <title>Index</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+        <title>Home</title>
     </head>
     <body>
         <!-- cabeçalho -->
@@ -11,11 +13,18 @@
 
         <!-- página -->
             <div class="container">
+                <div id="logo">
+                    <img src="assets/img/logo-fatec.png" alt="logo-fatec" class="img-responsive">
+                </div>
                 <h1 class="text-center">Trabalho Prático de Linguagem de Programação IV</h1>
             </div>
 
         <!-- rodapé -->
         <?php include 'footer.php' ?>
-    <script src="assets/js/bootstrap.min.js"></script>
+
+        <!-- scripts -->
+        <script src="assets/js/jquery.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
     </body>
 </html>
--- menu.php
+++ menu.php
@@ -6,22 +6,43 @@
  * Time: 19:45
  */
 ?>
-<nav class="navbar navbar-inverse navbar-toggleable-md bg-inverse">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">Fatec PG</a>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Página Inicial<span class="sr-only">(Current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">#</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">#</a>
-            </li>
-        </ul>
+<div class="navbar-wrapper">
+    <div class="container-fluid">
+        <nav class="navbar navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Menu</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                        <span class="sr-only">Menu</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div id="navbar" class="navbar-collapse">
+                    <ul class="nav" id="menu">
+                        <li class="col-md-4">
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li class="col-md-4">
+                            <a href="#about">
+                                About
+                            </a>
+                        </li>
+                        <li class="col-md-4">
+                            <a href="#contact">
+                                Contact
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
-</nav>
+</div>
