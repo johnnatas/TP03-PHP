951657121441cdd455b43fce65417d48dce064ce
Leonardo Santos <leonardossev@gmail.com>
2017-06-07 13:28:23 -0300

    [EDIT] Formulario

--- assets/includes/formulario.php
+++ assets/includes/formulario.php
@@ -8,29 +8,29 @@
 ?>
 
 <form action="" method="post">
-    <input type="text" name="nome" class="form-control formulario" />
-    <input type="text" name="email" class="form-control formulario" />
-    <input type="text" name="data" class="form-control formulario" />
-    <input type="text" name="num" class="form-control formulario" />
-    <textarea name="valtxt" class="form-control formulario"></textarea>
-    <textarea name="txtlinhas" class="form-control formulario"></textarea>
+    <input type="text" name="nome" class="form-control formulario" placeholder="Nome" required/>
+    <input type="text" name="email" class="form-control formulario" placeholder="E-mail" required/>
+    <input type="text" name="data" class="form-control formulario" placeholder="Data de Nascimento" max="31-12-1999" required/>
+    <input type="text" name="num" class="form-control formulario" placeholder="ID" required/>
+    <textarea name="comentario" class="form-control formulario" placeholder="Escreva um comentário"></textarea>
     <select name="dropdown" class="form-control formulario">
-        <option value="umacoisa">Uma Coisa</option>
-        <option value="duascoisas">Duas Coisas</option>
-        <option value="trescoisas">Três Coisas</option>
-        <option value="quatocoisas">Quatro Coisas</option>
+        <option value="Análise e Desenvolvimento de Sistemas">Análise e Desenvolvimento de Sistemas</option>
+        <option value="Comércio Exterior">Comércio Exterior</option>
+        <option value="Gestão Empresarial">Gestão Empresarial</option>
+        <option value="Processos Químicos">Processos Químicos</option>
     </select>
     <div class="formulario">
-        <label class="radio-inline">
-            <input type="radio" name="rdb" value="1"/> 1
+        <label class="radio-inline" for="masculino">
+            <input type="radio" name="sexo" value="Masculino" id="msaculino"/> Masculino
         </label>
-        <label class="radio-inline">
-            <input type="radio" name="rdb" value="2"/> 2
-        </label>
-        <label class="radio-inline">
-            <input type="radio" name="rdb" value="3"/> 3
+        <label class="radio-inline" for="feminino">
+            <input type="radio" name="sexo" value="Feminino" id="feminino"/> Feminino
         </label>
     </div>
-
     <input type="submit" name="inserir" value="Inserir dados" class="btn formulario">
+</form>
+<form action="" method="post">
+    Ou cadastre-se por arquivo csv:
+    <input type="file" name="arquivo" id="arquivo">
+    <input type="submit" name="enviarCSV" value="Enviar" class="btn formulario">
 </form>
--- index.php
+++ index.php
@@ -7,35 +7,6 @@
         <title>Index</title>
     </head>
     <body>
-    <div class="container">
-        <!--MENU-->
-        <nav class="navbar navbar-inverse navbar-toggleable-md bg-inverse">
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">Fatec PG</a>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Página Inicial<span class="sr-only">(Current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="form.php">Formulário</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">#</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        <!--FIM-MENU-->
-        <div class="container">
-            <h1 class="text-center">Trabalho Prático de Linguagem de Programação IV</h1>
-
-
-        </div>
-
-    <script src="assets/js/bootstrap.min.js"></script>
 =======
 <?php include 'assets/includes/header.php' ?>
         <!-- cabeçalho -->
