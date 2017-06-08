9693f21e265e9a20618d27e5319bc23fba09f543
Leonardo Santos <leonardossev@gmail.com>
2017-06-08 18:46:28 -0300

    CSV AINDA NAO FUNCIONA

--- enviarCSV.php
+++ enviarCSV.php
@@ -10,12 +10,13 @@ require_once 'Classes/Aluno.php';
 
 try{
     $status = move_uploaded_file($_FILES["aluno-csv"]["tmp_name"], "files/" . time() . "-" . $_FILES["aluno-csv"]["name"]);
+    var_dump($_FILES);
     if ($status) {
         $f = fopen("files/" . time() . "-" . $_FILES["aluno-csv"]["name"], "r");
         if ($f) {
             $title_row = fgetcsv($f, 100, ";");
             $rows = fgetcsv($f, 100, ";");
-            while ($rows !== null) {
+            while ($rows !== false) {
                 $aluno = new Aluno($rows[1], $rows[2], $rows[3], $rows[4], $rows[5], $rows[6], $rows[7]);
                 $aluno->create();
                 $rows = fgetcsv($f, 100, ";");
