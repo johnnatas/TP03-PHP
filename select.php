ca0a376c0d1d75dae68ba25125c2b156a295759d
Leonardo Santos <leonardossev@gmail.com>
2017-06-08 10:27:10 -0300

    [EDIT] Trocando echo por HTML

--- select.php
+++ select.php
@@ -9,30 +9,32 @@ $con = getConnection();
 $SQL = "SELECT ID, NOME, EMAIL, DATA, COMENTARIO, CURSO, SEXO  FROM tb_aluno";
 $tab = mysqli_query($con, $SQL);
 $linha = mysqli_fetch_assoc($tab);
-
-echo "<div class='table-responsive'>";
-echo "    <table class='table'>";
-echo "        <tr>";
-echo "            <th>ID</th>";
-echo "                  <th>Nome</th>";
-echo "                  <th>E-mail</th>";
-echo "            <th>Data de Nascimento</th>";
-echo "                  <th>Comentário</th>";
-echo "            <th>Curso</th>";
-echo "            <th>Sexo</th>";
-echo "             </tr>";
-
+?>
+<div class='table-responsive'>
+    <table class='table'>
+        <tr>;
+    <th>ID</th>
+    <th>Nome</th>
+    <th>E-mail</th>
+    <th>Data de Nascimento</th>
+    <th>Comentário</th>
+    <th>Curso</th>
+    <th>Sexo</th>
+</tr>
+<?
 while($linha != null) {
-    echo "         <tr>";
-    echo "             <td>" . $linha["ID"] . "</td>";
-    echo "             <td>" . $linha["NOME"] . "</td>";
-    echo "             <td>" . $linha["EMAIL"] . "</td>";
-    echo "             <td>" . $linha["DATA"] . "</td>";
-    echo "             <td>" . $linha["COMENTARIO"] . "</td>";
-    echo "             <td>" . $linha["CURSO"] . "</td>";
-    echo "             <td>" . $linha["SEXO"] . "</td>";
-    echo "             <td><a href='editar.php?id={$linha['ID']}'><button>Editar</button></a>";
-    echo "             <td><a href='excluir.php?id={$linha['ID']}'><button>Excluir</button></a>";
-    echo "         </tr>";
-    $linha = mysqli_fetch_assoc($tab);
+    ?>
+    <tr>
+        <td><?=$linha["ID"]?></td>
+        <td><?=$linha["NOME"]?></td>
+        <td><?=$linha["EMAIL"]?></td>
+        <td><?=$linha["DATA"]?></td>
+        <td><?=$linha["COMENTARIO"]?></td>
+        <td><?=$linha["CURSO"]?></td>
+        <td><?=$linha["SEXO"]?></td>
+        <td><a href='editar.php?id=<?=$linha['ID']?>'><button>Editar</button></a>
+        <td><a href='excluir.php?id=<?=$linha['ID']?>'><button>Excluir</button></a>
+    </tr>
+    <?$linha = mysqli_fetch_assoc($tab);
 }
+?>
