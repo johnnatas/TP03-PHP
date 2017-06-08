3051f2f4fec4ae238da67514bcd9950492c2fa36
Leonardo Santos <leonardossev@gmail.com>
2017-06-08 07:14:53 -0300

    [EDIT] valores dos campos e removendo botões.

--- assets/includes/formulario.php
+++ assets/includes/formulario.php
@@ -6,14 +6,12 @@
  * Time: 14:08
  */
 ?>
-
-<form action="" method="post">
-    <input type="text" name="num" class="form-control formulario" placeholder="ID" required/>
-    <input type="text" name="nome" class="form-control formulario" placeholder="Nome" required/>
-    <input type="text" name="email" class="form-control formulario" placeholder="E-mail" required/>
-    <input type="date" name="data" class="form-control formulario" placeholder="Data de Nascimento" max="1999-12-31" required/>
-    <textarea name="comentario" class="form-control formulario" placeholder="Escreva um comentário"></textarea>
-    <select name="curso" class="form-control formulario">
+    <input type="text" name="id" id="nome" class="form-control formulario" placeholder="ID" value="<?=$id?>"required/>
+    <input type="text" name="nome" id="nome" class="form-control formulario" placeholder="Nome" value="<?=$id?>" required/>
+    <input type="text" name="email" id="email" class="form-control formulario" placeholder="E-mail" value="<?=$id?>" required/>
+    <input type="date" name="data" id="data" class="form-control formulario" placeholder="Data de Nascimento" max="1999-12-31" value="<?=$id?>" required/>
+    <textarea name="comentario" id="comentario" class="form-control formulario" placeholder="Escreva um comentário"> <?=$id?></textarea>
+    <select name="curso" id="curso" class="form-control formulario">
         <option value="Análise e Desenvolvimento de Sistemas">Análise e Desenvolvimento de Sistemas</option>
         <option value="Comércio Exterior">Comércio Exterior</option>
         <option value="Gestão Empresarial">Gestão Empresarial</option>
@@ -21,18 +19,18 @@
     </select>
     <div class="formulario">
         <label class="radio-inline" for="masculino">
-            <input type="radio" name="sexo" value="M" id="msaculino"/> Masculino
+            <input type="radio" name="sexo" id="sexo" value="Masculino" id="msaculino"/> Masculino
         </label>
         <label class="radio-inline" for="feminino">
-            <input type="radio" name="sexo" value="F" id="feminino"/> Feminino
+            <input type="radio" name="sexo" id="sexo" value="Feminino" id="feminino"/> Feminino
         </label>
     </div>
-    <input type="submit" name="inserir" value="Inserir dados" class="btn formulario">
-</form>
-<form action="enviarCSV.php" method="post">
+
+<!--
+<form action="enviarCSV.php" method="POST">
     <div class="formulario">
         Ou cadastre-se por arquivo csv:
         <input type="file" name="arquivo" id="arquivo">
         <input type="submit" name="enviarCSV" value="Enviar" class="btn formulario">
     </div>
-</form>
+-->
