13afe012adde580caac39ef34ee155b5b9a1cb8d
Leonardo Santos <leonardossev@gmail.com>
2017-06-15 21:32:01 -0300

    Mudando pattern dos inputs e values.

--- inserir.php
+++ inserir.php
@@ -2,6 +2,11 @@
     include 'assets/includes/header.php';
     include 'assets/includes/menu.php';
     include 'assets/includes/session_cookies.php';
+    $id="";
+    $nome="";
+    $email="";
+    $data="";
+    $comentario="";
 ?>
 
     <div class="container">
@@ -10,13 +15,13 @@
                 <h1>Insira os dados aqui</h1>
 
                 <form action="insereBanco.php" method="POST" id="inserir">
-                     <input type="text" name="id" id="id" class="form-control formulario" placeholder="ID" value="<?= isset($_SESSION["id"])?$_SESSION["id"]:"" ?>" pattern="[0-9]+$" required/>
+                     <input type="text" name="id" id="id" class="form-control formulario" placeholder="ID" value="<?= isset($_SESSION["id"])?$_SESSION["id"]:$id ?>" pattern="[0-9]+$" required/>
                     <?php include_once 'assets/includes/formulario.php'; ?>
                     <input type="submit" name="inserir" value="Inserir dados" class="btn formulario">
                 </form>
                 <form action="enviarCSV.php" method="POST" enctype="multipart/form-data" id="enviarCSV">
                         Ou cadastre-se por arquivo csv:
-                        <input name="csv" type="file" accept="text/csv"  id="csv">
+                        <input name="csv" type="file" accept="text/csv"  id="csv" required>
                         <input type="submit" name="enviarCSV" value="Enviar" class="btn formulario">
                 </form>
             </div>
