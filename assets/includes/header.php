6710ef141d65f84dab4036e7fb3bb23467fea61b
Leonardo Santos <leonardossev@gmail.com>
2017-06-17 01:25:11 -0300

    Imagens dos desenvolvedores

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -14,6 +14,7 @@
     <link rel="shortcut icon" href="../img/icone-fatec.png" type="image/x-icon" />
     <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/card.css" type="text/css">
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
     <title>TP03-PHP</title>
 </head>
--- index.php
+++ index.php
@@ -18,11 +18,52 @@
                         include_once "tabela-dados.php";
                     ?>
                 </div>
-                <h2 class="text-center">Mapa</h2>
-                <div class="col-md-12 offset-2">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7382408817375!2d-46.4145246845655!3d-24.005018784011334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1spt-BR!2sbr!4v1497569560683" width="700" height="450" frameborder="1"></iframe>
-                </div>
 
+                <h2 class="text-center">Mapa</h2>
+                <div class="row">
+                    <div class="col-md-12 offset-2">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3644.7382408817375!2d-46.4145246845655!3d-24.005018784011334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1db2e586da8d%3A0x271ae3e10bdc671e!2sFATEC!5e0!3m2!1spt-BR!2sbr!4v1497569560683" width="700" height="450" frameborder="1"></iframe>
+                    </div>
+                </div>
+                <div class="divider"></div>
+                <h2 class="text-center">Desenvolvedores</h2>
+                <div class="row">
+                    <div class="col-md-3 col-md-offset-2">
+                        <div class="card hovercard">
+                            <div id="cardheaderJonatas">
+                            </div>
+                            <div class="avatar">
+                                <img alt="Jonatas Lopes" src="assets/img/jonatas.jpg"></div>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="#">Jonatas Lopes</a>
+                                </div>
+                                <div class="desc">Estilização do projeto</div>
+                                <div class="desc">Estrutura do projeto</div>
+                                <div class="desc">Ajax</div>
+                                <div class="desc">Banco de Dados</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-md-offset-2">
+                        <div class="card hovercard">
+                            <div id="cardheaderLeonardo"></div>
+                            <div class="avatar">
+                                <img alt="Leonardo Santos" src="assets/img/leonardo.jpg">
+                            </div>
+                            <div class="info">
+                                <div class="title">
+                                    <a href="#">Leonardo Santos</a>
+                                </div>
+                                <div class="desc">CRUD</div>
+                                <div class="desc">Exibição dos dados</div>
+                                <div class="desc">Formulário</div>
+                                <div class="desc">CSV</div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
             </div>
 
         <!-- rodapé -->
